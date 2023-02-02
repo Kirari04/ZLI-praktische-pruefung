3d5e372d2538f0c329f580135bbd435cec31c8bd
Lev Saminskij <103888491+Kirari04@users.noreply.github.com>
2023-02-02 10:37:46 +0000

    added menu routes

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -5,37 +5,36 @@ import {
     MenuList,
     MenuItem,
     IconButton,
+    Stack,
 } from "@chakra-ui/react";
 
-import {
-    HamburgerIcon,
-    AddIcon,
-    ExternalLinkIcon,
-    EditIcon,
-    RepeatIcon,
-} from "@chakra-ui/icons";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export default function DefaultLayout() {
     return (
         <>
-            <h1>Willkommen zu ...</h1>
-            <Menu>
-                <MenuButton
-                    as={IconButton}
-                    aria-label="Options"
-                    icon={<HamburgerIcon />}
-                    variant="outline"
-                />
-                <MenuList>
-                    <Link to={"/"}>
-                        <MenuItem>Home</MenuItem>
-                    </Link>
-                    <Link to={"/work"}>
-                        <MenuItem>Work</MenuItem>
-                    </Link>
-                </MenuList>
-            </Menu>
-            <Outlet />
+            <Stack m={4} gap={4} direction="column" align="start">
+                <Menu>
+                    <MenuButton
+                        as={IconButton}
+                        aria-label="Options"
+                        icon={<HamburgerIcon />}
+                        variant="outline"
+                    />
+                    <MenuList>
+                        <Link to={"/"}>
+                            <MenuItem>Home</MenuItem>
+                        </Link>
+                        <Link to={"/aufgaben"}>
+                            <MenuItem>Alle Aufgaben</MenuItem>
+                        </Link>
+                        <Link to={"/aufgabe/hinzufuegen"}>
+                            <MenuItem>Aufgaben Hinzufügen</MenuItem>
+                        </Link>
+                    </MenuList>
+                </Menu>
+                <Outlet />
+            </Stack>
         </>
     );
 }
