e8624566860c4ed39bd523092b2c5379e45df735
Lev Saminskij <103888491+Kirari04@users.noreply.github.com>
2023-02-02 10:38:16 +0000

    added singular interface

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -5,8 +5,14 @@ interface WorkItem {
     title: string;
     completed: boolean;
 }
+
 interface LoadWorkItems {
     success: boolean;
     data: Array<WorkItem>;
+    error: string | null;
+}
+interface LoadWorkItem {
+    success: boolean;
+    data: WorkItem | null;
     error: string | null;
 }
