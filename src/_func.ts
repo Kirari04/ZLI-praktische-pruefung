8718797200567f904e435f06466d7305ab9e7ddc
Lev Saminskij <103888491+Kirari04@users.noreply.github.com>
2023-02-02 10:37:26 +0000

    added crud functions

--- src/_func.ts
+++ src/_func.ts
@@ -1,22 +1,142 @@
-import _data from "./_data"
+import axios from "axios";
+import _data from "./_data";
 
-export default function loadWorkList(path: string, isJson: boolean = true): Promise<LoadWorkItems> {
- return new Promise((resolve, reject) => {
-    fetch(new URL(`${_data.API}${path}`))
-    .then(e => isJson?e.json():e.text())
-    .then(data => {
-        resolve({
-            success: true,
-            data: data,
-            error: null
-        })
-    })
-    .catch(err => {
-        resolve({
-            success: false,
-            data: [],
-            error: err.message
-        })
-    })
- })
+const globalConfig = {
+    timeout: 5000
 }
+
+function LoadWorkItems_fn(
+    path: string,
+): Promise<LoadWorkItems> {
+    return new Promise((resolve, reject) => {
+        axios.get(`${_data.API}${path}`, globalConfig)
+            .then((data) => {
+                resolve({
+                    success: true,
+                    data: data.data,
+                    error: null,
+                });
+            })
+            .catch((err) => {
+                resolve({
+                    success: false,
+                    data: [],
+                    error: err.message,
+                });
+            });
+    });
+}
+
+function loadWorkItem_fn(
+    path: string,
+): Promise<LoadWorkItem> {
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`${_data.API}${path}`, globalConfig)
+            .then((data) => {
+                resolve({
+                    success: true,
+                    data: data.data,
+                    error: null,
+                });
+            })
+            .catch((err) => {
+                resolve({
+                    success: false,
+                    data: null,
+                    error: err.message,
+                });
+            });
+    });
+}
+function updateWorkItem_fn(
+    path: string,
+    id: number,
+    completed: boolean,
+    title: string
+): Promise<LoadWorkItem> {
+    return new Promise((resolve, reject) => {
+        axios
+            .put(`${_data.API}${path}`, {
+                id: id,
+                completed: completed,
+                title: title
+            }, globalConfig)
+            .then((data) => {
+                resolve({
+                    success: true,
+                    data: {
+                        id: id,
+                        completed: completed,
+                        title: title
+                    },
+                    error: null,
+                });
+            })
+            .catch((err) => {
+                resolve({
+                    success: false,
+                    data: null,
+                    error: err.message,
+                });
+            });
+    });
+}
+
+function deleteWorkItem_fn(
+    path: string,
+    id: number,
+): Promise<LoadWorkItem> {
+    return new Promise((resolve, reject) => {
+        axios
+            .delete(`${_data.API}${path}/${id}`, globalConfig)
+            .then((data) => {
+                resolve({
+                    success: true,
+                    data: null,
+                    error: null,
+                });
+            })
+            .catch((err) => {
+                resolve({
+                    success: false,
+                    data: null,
+                    error: err.message,
+                });
+            });
+    });
+}
+
+function addWorkItem_fn(
+    path: string,
+    completed: boolean,
+    title: string
+): Promise<LoadWorkItem> {
+    return new Promise((resolve, reject) => {
+        axios
+            .post(`${_data.API}${path}`, {
+                completed: completed,
+                title: title
+            }, globalConfig)
+            .then((data) => {
+                resolve({
+                    success: true,
+                    data: {
+                        id: data.data.id,
+                        completed: data.data.completed,
+                        title: data.data.title
+                    },
+                    error: null,
+                });
+            })
+            .catch((err) => {
+                resolve({
+                    success: false,
+                    data: null,
+                    error: err.message,
+                });
+            });
+    });
+}
+
+export { LoadWorkItems_fn, loadWorkItem_fn, updateWorkItem_fn, addWorkItem_fn, deleteWorkItem_fn };
