ad52b5d846dfb5a34c3480995a71c04038736ee3
Lev Saminskij <103888491+Kirari04@users.noreply.github.com>
2023-02-03 14:21:56 +0000

    added home page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,47 @@
+import { Button, Code, Heading, Spinner } from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Auth from "../hooks/_auth";
+import { UserContext } from "../hooks/_state";
+
 export default function Index() {
+    const navigate = useNavigate();
+    const [isLoading, setisLoading] = useState(false);
+    const [email, setemail] = useState<null | string>(null);
+    const { isAuth, setIsAuth } = useContext(UserContext);
+
+    useEffect(() => {
+        setisLoading(true);
+        new Auth(
+            (e: Auth) => {
+                setemail(e.user.email);
+                setisLoading(false);
+                if (!e.isAuth) {
+                    navigate("/login");
+                }
+            },
+            isAuth,
+            setIsAuth,
+            true
+        );
+    }, []);
+
     return (
         <>
-            <h1>Home</h1>
+            <Heading as="h1">Home</Heading>
+            {isLoading ? <Spinner /> : null}
+            {isAuth ? (
+                <Heading as="p" size="md">
+                    Wellcome back <Code>{`${email}`}</Code>
+                </Heading>
+            ) : (
+                <Heading as="p" size="md">
+                    Melde dich jetzt an:{" "}
+                    <Link to={"/login"}>
+                        <Button colorScheme={"blue"}>Anmelden</Button>
+                    </Link>
+                </Heading>
+            )}
         </>
     );
 }
